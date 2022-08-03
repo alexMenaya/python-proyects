7ee2d40512122fffdf5ec00165005e972c56c5c5
Alex Menaya <alexmenaya@gmail.com>
2022-08-03 22:41:02 +0200

    Counting principal diagonals, that worked. Next step is secondary diagonals

--- TicTacToe.py
+++ TicTacToe.py
@@ -38,7 +38,7 @@ class Board:
     def updateBoard(self, player, coordinate): 
         row = (coordinate - 1) //self.width
         col = (coordinate - 1) % self.width
-        self.board[row][col] = player.token
+        self.board[row][col] = player
         return self
         
     def draw(self):
@@ -82,14 +82,15 @@ class MNKGame:
         self.activePlayer = self.selectRandomPlayer()
         self.toWin = toWin
         self.isThereWinner = False
+        self.draw = False
     
     def selectRandomPlayer(self):
         r = randint(0,1)
         return self.player1.token*r + self.player2.token*(1-r)
     
     def swapPlayerTurn(self, player):
-        r = self.player1.token == player.token
-        self.activePlayer(self.player1.token*r + self.player2.token*(1-r))
+        r = self.player1.token == player
+        self.activePlayer = self.player1.token*r + self.player2.token*(1-r)
         return self
     
     def isIterableFull(self, iterable):
@@ -111,94 +112,59 @@ class MNKGame:
         # Check horizontal: each row
         all_spots_hor = range(max(0,col-self.toWin + 1),min(col+self.toWin,self.width))
         num_spots_hor = len(all_spots_hor) - self.toWin + 1
+        print("horizontal " +str(num_spots_hor))
         for index_col in range(num_spots_hor):
-            to_check = [bo[row, max(0,col-self.toWin + 1)+index_col + k] for k in range(self.toWin)]
-            kInLine = kInLine or (self.isIterableFull(to_check) and self.allIterableEqual(to_check))
+            to_check = [[row, max(0,col-self.toWin + 1)+index_col + k] for k in range(self.toWin)]
+            #print(to_check)
+            #kInLine = kInLine or (self.isIterableFull(to_check) and self.allIterableEqual(to_check))
         # Check veritcal
         all_spots_ver = range(max(0,row-self.toWin + 1),min(row+self.toWin,self.height))
         num_spots_ver = len(all_spots_ver) - self.toWin + 1
+        print("vertical "+str(num_spots_ver))
         for index_row in range(num_spots_ver):
-            to_check = [bo[max(0,row-self.toWin + 1)+index_row + k, col] for k in range(self.toWin)]
-            kInLine = kInLine or (self.isIterableFull(to_check) and self.allIterableEqual(to_check))
+            to_check = [[max(0,row-self.toWin + 1)+index_row + k, col] for k in range(self.toWin)]
+            #print(to_check)
+            #kInLine = kInLine or (self.isIterableFull(to_check) and self.allIterableEqual(to_check))
         # Check principal diagonal
-        number_of_pd = min(max(0,self.toWin -abs(row-col)), min(row, self.height-row) + 1, min(col,self.width-col) + 1)
+        number_of_pd = ((self.width-(row-col)>=self.toWin-1) and (self.width-(col-row)>=self.toWin-1))*min(row+1, col+1, self.height-row, self.width-col, self.width-(row-col)-self.toWin+1,self.width-(col-row)-self.toWin+1, self.toWin)
+        print("principal diagonal " + str(number_of_pd))
         for ind in range(number_of_pd):
-            to_check = [bo[row- (number_of_pd -1) + ind + k,col-(number_of_pd-1) + ind + k] for k in range(self.toWin)]
-            kInLine = kInLine or (self.isIterableFull(to_check) and self.allIterableEqual(to_check))
+            #This is not working 
+            to_check = [[max(0, row-self.toWin) + ind + k, max(0, col-self.toWin) + ind + k] for k in range(self.toWin)]
+            print(to_check)
+            #kInLine = kInLine or (self.isIterableFull(to_check) and self.allIterableEqual(to_check))
         # Check secondary diagonal
-        number_of_sd = min(max(0,self.toWin -abs(row+col-self.width)), min(row, self.height-row) + 1, min(col,self.width-col) + 1)
+        # Clean next line (because i changed j -> len -j and we can cancel stuff)
+        number_of_sd = ((self.width-(row-(self.width-col-1))>=self.toWin-1) and (self.width-((self.width-col-1)-row)>=self.toWin-1))*min(row+1, (self.width-col-1)+1, self.height-row, self.width-(self.width-col-1), self.width-(row-(self.width-col-1))-self.toWin+1,self.width-((self.width-col-1)-row)-self.toWin+1, self.toWin)
+        print(" antidiagonal " + str(number_of_sd))
         for ind in range(number_of_sd):
-            to_check = [bo[row - (number_of_sd -1) + ind + k,col + (number_of_sd -1) -ind -k] for k in range(self.toWin)]
-            kInLine = kInLine or (self.isIterableFull(to_check) and self.allIterableEqual(to_check))
+            # This is not right
+            to_check = [[self.height + (max(0, row-self.toWin) - ind - k) - 1, self.width + (max(0, (self.width-col-1)-self.toWin) - ind - k) -1] for k in range(self.toWin)]
+            print(to_check)
+            #kInLine = kInLine or (self.isIterableFull(to_check) and self.allIterableEqual(to_check))
         return kInLine
         
     def play(self):
         # Reset board
         self.board.resetBoard()
-        while not self.isThereWinner:
-            print("It is turn for player: " + self.activePlayer())
+        self.isThereWinner = False
+        self.draw = False
+        while not self.isThereWinner or self.draw:
+            print("It is turn for player: " + self.activePlayer)
             # Show the board
             self.board.draw()
             # Ask user for a spot
-            inputSpot = int(input("Choose a spot < " + self.width*self.height+1 + ": "))
-            while self.board.isSpotFull(inputSpot):
-                inputSpot = int(input("Invalid spot. Choose an empty spot < " + self.width*self.height+1 + ": "))
+            spots = str(self.width*self.height+1)
+            inputSpot = int(input("Choose a spot < " + spots + ": "))
+            while self.board.isSpotFull(inputSpot) or inputSpot > self.width*self.height+1 or inputSpot < 0:
+                inputSpot = int(input("Invalid spot. Choose an empty spot < " + spots + ": "))
             self.board.updateBoard(self.activePlayer, inputSpot)
             self.isThereWinner = self.kInLine(inputSpot)
+            self.draw = self.board.isBoardFull()
             self.swapPlayerTurn(self.activePlayer)
         
         self.swapPlayerTurn(self.activePlayer)
-        print("Winner is player " + self.activePlayer)
-        
-     
-    # def play(self):
-    #     # Reset board
-    #     self.resetBoard()
-    #     # Choose player
-    #     player = self.startingPlayer()
-    #     while True:
-    #         print("It is turn of player " + player)
-    #         # Show the board
-    #         self.draw()
-    #         # Ask user for a spot
-    #         invalidSpot = True
-    #         while invalidSpot:
-    #             input_row, input_col = map(int, input("Choose a spot x y| 0<=x,y<=2: " ).split())
-    #             if self.board[input_row, input_col] == BLANK:
-    #                 self.board[input_row, input_col] = player
-    #                 invalidSpot = False;
-    #             else :
-    #                 input_row, input_col = map(int, input("Invalid spot. Choose a spot (x,y)| 0<=x,y<=2: " ).split())
-            
-    #         # check if there is a line, break
-    #         if self.threeInLine():
-    #             print("Winner is player " + player)
-    #             self.draw()
-    #             break
-    #         # check if the board is empty
-    #         if self.isBoardFull():
-    #             print("Draw")
-    #             self.draw()
-    #             break
-    #         player = self.swapPlayer(player)
-        
-    # def threeInLine(self):
-    #     """ Returns True if there is a line full with the same token"""
-    #     b = self.board
-        
-    #     # Check rows b[row_num,:]
-    #     for row in range(self.size):
-    #         if self.isIterableFull(b[row,:]) and self.allIterableEqual(b[row,:]):
-    #             return True
-    #     # Check columns b[:,column]
-    #     for col in range(self.size):
-    #         if self.isIterableFull(b[:,col]) and self.allIterableEqual(b[:,col]):
-    #             return True
-    #     # Check diagonals
-    #     principal = [b[k][k] for k in range(self.size)]
-    #     secondary = [b[k][-(k+1)] for k in range(self.size)]
-    #     if (self.isIterableFull(principal) and self.allIterableEqual(principal)):
-    #         return True
-    #     if (self.isIterableFull(secondary) and self.allIterableEqual(secondary)):
-    #         return True
-    #     return False
+        msgWinner = "Winner is player " + self.activePlayer
+        msgDraw = "Board is full, game ends in a draw"
+        msg = msgWinner*self.isThereWinner + msgDraw*self.board.draw
+        print(msg)
